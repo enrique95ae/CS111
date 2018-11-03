65f285c6fe0adcd83f99167569aab7c9d7dd9002
enrique95ae <34976400+enrique95ae@users.noreply.github.com>
2018-11-02 22:23:18 -0700

    Update 2darrays.cpp

--- LABS/2darrays.cpp
+++ LABS/2darrays.cpp
@@ -7,15 +7,12 @@ void fillArray(double grades[][COL])
 {
   ifstream inFile;
   inFile.open("2dinput.txt");
-  for(int k=0; k<ROW; k++)
-        {
-        for(int c=0; c<COL; c++)
-        {
+  for(int k=0; k<ROW; k++){
+          for(int c=0; c<COL; c++){
         inFile >> grades[k][c];
         }
-        }
+  }
   inFile.close();
-
 }
 
 void higherThan90(double grades[][COL])
@@ -24,34 +21,25 @@ void higherThan90(double grades[][COL])
 
   double add;
 
-  for(int k=0; k<ROW; k++)
-        {
-
-        for (int c=0; c<COL; c++)
-        {
-        add+= grades[k][c];
+  for(int k=0; k<ROW; k++) {
+        for (int c=0; c<COL; c++){
+           add+= grades[k][c];
         }
       average[k]=add/3.0;
         add=0;
-        }
-
-
+  }
 
   ofstream outFile;
   outFile.open("2doutput.txt");
 
-  for(int k=0; k<ROW; k++)
-        {
-        if (average[k] >= 90)
-        {
-
-        for (int c=0; c<COL; c++)
-        {
-        outFile << grades[k][c]<< " ";
+  for(int k=0; k<ROW; k++) {
+      if (average[k] >= 90){
+        for (int c=0; c<COL; c++){
+          outFile << grades[k][c]<< " ";
         }
-        outFile << average[k] << endl;
-        }
-        }
+          outFile << average[k] << endl;
+      }
+  }
 }
 
 int main()
