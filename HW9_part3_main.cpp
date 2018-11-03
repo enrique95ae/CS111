c088b05dd1c35270d552e28d4f80b963e8baa0e1
enrique95ae <34976400+enrique95ae@users.noreply.github.com>
2018-11-02 22:08:22 -0700

    Update and rename main 3.cpp to HW9_part3_main.cpp

--- HW9_part3_main.cpp
+++ HW9_part3_main.cpp
@@ -25,14 +25,16 @@ int main() {
     int cont;
     
     while(base4==false){ //loop to check if the number is base 4. We split is as in HW9 Question 1 and then we check if each number is between 0-3
+        
         if(v!=0){
             cout << "Enter a valid base 4 number!" << endl ;
         }
+        
         cout << "Enter a number base 4: " ;
         cin >> n ;
         cout << endl ;
-        v++;
         
+        v++;
         aux=n;
         aux2=n;
         
@@ -43,15 +45,11 @@ int main() {
         
          //Array that will store the numbers of the interger. Need an array for later checking if the numbers are in the range 0-3
         
-        
         for(int i=0; i<lenght ; i++){ //loop in which we store the las number of the interget in the array and we take it away from the variable number.
             num[i]=n%10; //storing the last number from the interger in the array.
             n=n/10; //Taking that alredy stored number away.
-
         }
-        
- 
-        
+       
         for(int i=0; i<lenght ; i++){
             if(num[i] == 0 || num[i] == 1 || num[i] == 2 || num[i] == 3){
                 base4=true; //if is base 4 we get out of the validation loop and start the conversion.
@@ -62,18 +60,11 @@ int main() {
     }
     
     cont=lenght;
-
     
     for(int i=cont-1; i>=0; i--){ //flipping the number (it was stored reversed in the array);
-
         num[i]=aux2%10; //storing the last number from the interger in the array.
         aux2=aux2/10; //Taking that alredy stored number away.
     }
-    
-
-    
-
-
     
     int x = lenght;
     x = x-1;
@@ -87,12 +78,8 @@ int main() {
         if(x<1){
             newNum = num[i];
             base10 = base10 + newNum;
-        }
-        
+        } 
     }
     
-
     cout << base10;
-    
-
 }
